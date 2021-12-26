83b0e9dc4c195570cb00cfcbef73040f97d52a88
louis2sin <louis2sin@gmail.com>
2021-12-26 23:07:45 +0800

    Project Initialization

---
+++ crypto/src/main/java/com/silong/fundation/crypto/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Encryption and decryption toolkit, support commonly used encryption and decryption algorithms.
+ *
+ * <p>It features enhanced security by working together with the root key and the working key.
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2021-12-26 23:00
+ */
+package com.silong.fundation.crypto;
