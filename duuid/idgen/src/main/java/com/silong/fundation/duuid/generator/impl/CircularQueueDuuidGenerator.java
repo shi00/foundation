afb99d48ce11d1270c40ce2313a49919056bbbdd
louis2sin <louis2sin@gmail.com>
2022-01-02 17:57:21 +0800

    Project Initialization

--- duuid/idgen/src/main/java/com/silong/fundation/duuid/generator/impl/CircularQueueDuuidGenerator.java
+++ duuid/idgen/src/main/java/com/silong/fundation/duuid/generator/impl/CircularQueueDuuidGenerator.java
@@ -228,13 +228,13 @@ public class CircularQueueDuuidGenerator implements DuuidGenerator {
     queue.fill(
         this::generate,
         idleCounter -> {
-          try {
-            // 计算当前队列填充率，如果大于等于阈值则放弃CPU时间片，马上重新竞争时间片，给其他任务执行机会
-            while ((queue.size() * 1.0 / queue.capacity()) >= paddingFactor) {
+          // 计算当前队列填充率，如果大于等于阈值则放弃CPU时间片，马上重新竞争时间片，给其他任务执行机会
+          while ((queue.size() * 1.0 / queue.capacity()) >= paddingFactor) {
+            try {
               Thread.sleep(1L);
+            } catch (InterruptedException e) {
+              // never happen in regular
             }
-          } catch (InterruptedException e) {
-            // never happen in regular
           }
           return idleCounter + 1;
         },
