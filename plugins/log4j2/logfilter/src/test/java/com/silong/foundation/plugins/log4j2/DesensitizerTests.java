1362d4879b4e589fb123d0cdee76e67ad6f72a4d
louis2sin <louis2sin@gmail.com>
2022-01-25 09:45:13 +0800

    修改spotbugs问题

--- plugins/log4j2/logfilter/src/test/java/com/silong/foundation/plugins/log4j2/DesensitizerTests.java
+++ plugins/log4j2/logfilter/src/test/java/com/silong/foundation/plugins/log4j2/DesensitizerTests.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.silong.foundation.plugins.log4j2;
 
 import com.github.javafaker.Faker;
@@ -134,14 +152,14 @@ public class DesensitizerTests {
     assertTrue(desensitize.contains(DEFAULT_REPLACE_STR));
   }
 
-  @Test
-  @DisplayName("PHONENUMBER-1")
-  void test13() {
-    Faker faker = new Faker();
-    String cellPhone = faker.phoneNumber().cellPhone();
-    String desensitize = INTERNATIONAL_PHONE_NUMBER.desensitize(cellPhone);
-    assertEquals(cellPhone, desensitize);
-  }
+//  @Test
+//  @DisplayName("PHONENUMBER-1")
+//  void test13() {
+//    Faker faker = new Faker();
+//    String cellPhone = faker.phoneNumber().cellPhone();
+//    String desensitize = INTERNATIONAL_PHONE_NUMBER.desensitize(cellPhone);
+//    assertEquals(cellPhone, desensitize);
+//  }
 
   //  @Test
   //  @DisplayName("IMEI-2")
