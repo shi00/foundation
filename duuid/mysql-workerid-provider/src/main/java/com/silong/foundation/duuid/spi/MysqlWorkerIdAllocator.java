eb1f86285bc2fb4981c31f98d354ceecdd380ef4
louis2sin <louis2sin@gmail.com>
2022-01-30 00:20:12 +0800

    新增mysql workerid分配器实现

---
+++ duuid/mysql-workerid-provider/src/main/java/com/silong/foundation/duuid/spi/MysqlWorkerIdAllocator.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.silong.foundation.duuid.spi;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Map;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+/**
+ * 基于ETCD v3的WorkerId分配器<br>
+ * 通过key的多版本属性，以key的版本号作为workId
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2022-01-03 00:25
+ */
+@Slf4j
+public class MysqlWorkerIdAllocator implements WorkerIdAllocator {
+
+  private static final String INSERT_SQL =
+      "INSERT INTO WORKER_ID_ALLOCATOR ( HOST_NAME ) VALUES ( ? )";
+
+  /** 建表脚本 */
+  public static final String CREATE_TABLE_SQL =
+      "CREATE TABLE IF NOT EXISTS WORKER_ID_ALLOCATOR"
+          + "(ID        BIGINT       NOT NULL AUTO_INCREMENT COMMENT 'auto increment WORKER_ID',"
+          + "HOST_NAME  VARCHAR(128) NOT NULL COMMENT 'host name',"
+          + "CREATED    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'created time',"
+          + "PRIMARY KEY (ID)) "
+          + "COMMENT='Worker ID Allocator for DUUID Generator',ENGINE = INNODB DEFAULT CHARSET=utf8mb4;";
+
+  /** jdbc url */
+  public static final String JDBC_URL = "jdbc.url";
+
+  /** 用户 */
+  public static final String USER = "user";
+
+  /** 用户密码 */
+  public static final String PASSWORD = "password";
+
+  /** jdbc驱动 */
+  public static final String JDBC_DRIVER = "jdbc.driver";
+
+  /** 主机名 */
+  public static final String HOST_NAME = "host.name";
+
+  @Override
+  public long allocate(WorkerInfo info) {
+    Map<String, String> extraInfo;
+    if (info == null || (extraInfo = info.getExtraInfo()) == null) {
+      throw new IllegalArgumentException("info and info.extraInfo must not be null.");
+    }
+    String password = getValueByKey(extraInfo, PASSWORD);
+    String jdbcDriver = getValueByKey(extraInfo, JDBC_DRIVER);
+    String userName = getValueByKey(extraInfo, USER);
+    String jdbcUrl = getValueByKey(extraInfo, JDBC_URL);
+    try (Connection connection = getConnection(jdbcDriver, jdbcUrl, userName, password);
+        PreparedStatement preparedStatement =
+            connection.prepareStatement(INSERT_SQL, RETURN_GENERATED_KEYS)) {
+      createTable(connection);
+      preparedStatement.setString(1, getValueByKey(extraInfo, HOST_NAME));
+      preparedStatement.executeUpdate();
+      try (ResultSet rs = preparedStatement.getGeneratedKeys()) {
+        if (rs.next()) {
+          return rs.getLong(1);
+        }
+        throw new IllegalStateException("Failed to get generated Key.");
+      }
+    } catch (SQLException | ClassNotFoundException | IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void createTable(Connection connection) throws IOException, SQLException {
+    try (PreparedStatement preparedStatement = connection.prepareStatement(CREATE_TABLE_SQL)) {
+      if (preparedStatement.execute()) {
+        log.info("The table of WORKER_ID_ALLOCATOR was created successfully.");
+      }
+    }
+  }
+
+  private static Connection getConnection(
+      String jdbcDriver, String jdbcUrl, String userName, String password)
+      throws ClassNotFoundException, SQLException {
+    Class.forName(jdbcDriver);
+    return DriverManager.getConnection(jdbcUrl, userName, password);
+  }
+
+  private String getValueByKey(Map<String, String> extraInfo, String key) {
+    String value = extraInfo.get(key);
+    if (isEmpty(value)) {
+      throw new IllegalArgumentException(
+          String.format("%s must not be null or empty in extraInfo.", key));
+    }
+    return value;
+  }
+
+  private boolean isEmpty(String str) {
+    return str == null || str.isEmpty();
+  }
+}
---
+++ duuid/mysql-workerid-provider/src/test/java/com/silong/foundation/duuid/spi/MysqlWorkerIdAllocatorTests.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.silong.foundation.duuid.spi;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Map;
+
+import static com.silong.foundation.duuid.spi.MysqlWorkerIdAllocator.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testcontainers.shaded.org.apache.commons.lang.SystemUtils.USER_NAME;
+
+/**
+ * 单元测试
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2022-01-28 22:56
+ */
+@Testcontainers
+public class MysqlWorkerIdAllocatorTests {
+
+  private static final String PASSWORD_TEST = "Abcd@1234";
+
+  private static final String USER_TEST = "root";
+
+  private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+  private static final MysqlWorkerIdAllocator ALLOCATOR = new MysqlWorkerIdAllocator();
+
+  @Container
+  private static final MySQLContainer MYSQL_CONTAINER =
+      new MySQLContainer("mysql:8.0.28")
+          .withDatabaseName("test")
+          .withUsername(USER_TEST)
+          .withPassword(PASSWORD_TEST);
+
+  private static Map<String, String> EXTRA_INFO;
+
+  @BeforeAll
+  @SneakyThrows
+  static void init() {
+    EXTRA_INFO =
+        Map.of(
+            JDBC_URL,
+            MYSQL_CONTAINER.getJdbcUrl(),
+            MysqlWorkerIdAllocator.JDBC_DRIVER,
+            JDBC_DRIVER,
+            PASSWORD,
+            PASSWORD_TEST,
+            USER,
+            USER_TEST,
+            HOST_NAME,
+            USER_NAME);
+  }
+
+  @Test
+  void test() {
+    for (int i = 1; i <= 100; i++) {
+      long allocate =
+          ALLOCATOR.allocate(WorkerInfo.builder().extraInfo(EXTRA_INFO).name(USER_NAME).build());
+      assertEquals(i, allocate);
+    }
+  }
+
+  @AfterAll
+  static void cleanUp() {
+    if (MYSQL_CONTAINER != null) {
+      MYSQL_CONTAINER.stop();
+    }
+  }
+}
