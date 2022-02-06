d986d8bd3607f5346ea7cf85ec3ab4472ad6b9de
louis2sin <louis2sin@gmail.com>
2022-02-06 14:08:45 +0800

    新增Webclient模块

--- reactive-webclient/src/test/resources/certs/generateKeys.bat
+++ reactive-webclient/src/test/resources/certs/generateKeys.bat
@@ -1,18 +1,31 @@
+REM 使用RSA算法生成双向认证证书
+
+::删除当前目录下已经生成的证书
 del *.p12 *.cer
 
-keytool -genkey -v -alias mockwebserver -keyalg RSA -keystore mockwebserver.p12 -storetype PKCS12 -validity 36500 -keysize 2048 -storepass password -dname "CN=localhost,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
+::指定生成证书的格式，文件名，主机名
+set STORE_TYPE=PKCS12
+set PASSWORD=password
+set HOST_NAME=localhost
+set SERVER=mockwebserver
+set CLIENT=client
+set VALIDITY=36500
+set KEYSIZE=2048
 
-keytool -genkey -v -alias client -keyalg RSA -keystore client.p12 -storetype PKCS12 -validity 36500 -keysize 2048 -storepass password -dname "CN=localhost,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
+::生成服务端证书
+keytool -genkey -v -alias %SERVER% -keyalg RSA -keystore %SERVER%.p12 -storetype %STORE_TYPE% -validity %VALIDITY% -keysize %KEYSIZE% -storepass %PASSWORD% -dname "CN=%HOST_NAME%,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
 
-keytool -export -alias client -keystore client.p12 -storetype PKCS12 -storepass password -rfc -file client.cer
+keytool -genkey -v -alias %CLIENT% -keyalg RSA -keystore %CLIENT%.p12 -storetype %STORE_TYPE% -validity %VALIDITY% -keysize %KEYSIZE% -storepass %PASSWORD% -dname "CN=%HOST_NAME%,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
 
-keytool -export -alias mockwebserver -keystore mockwebserver.p12 -storetype PKCS12 -storepass password -rfc -file mockwebserver.cer
+keytool -export -alias %CLIENT% -keystore %CLIENT%.p12 -storetype %STORE_TYPE% -storepass %PASSWORD% -rfc -file %CLIENT%.cer
 
-keytool -import -alias client -keystore mockwebserver-trust.p12 -storepass password -file client.cer
+keytool -export -alias %SERVER% -keystore %SERVER%.p12 -storetype %STORE_TYPE% -storepass %PASSWORD% -rfc -file %SERVER%.cer
 
-keytool -import -alias mockwebserver -keystore client-trust.p12 -storepass password -file mockwebserver.cer
+keytool -import -alias %CLIENT% -keystore %SERVER%-trust.p12 -storepass %PASSWORD% -file %CLIENT%.cer -noprompt
 
-keytool -list -v -keystore mockwebserver-trust.p12 -storepass password
-keytool -list -v -keystore client-trust.p12 -storepass password
-keytool -list -v -keystore mockwebserver.p12 -storepass password
-keytool -list -v -keystore client.p12 -storepass password
+keytool -import -alias %SERVER% -keystore %CLIENT%-trust.p12 -storepass %PASSWORD% -file %SERVER%.cer -noprompt
+
+keytool -list -v -keystore %SERVER%-trust.p12 -storepass %PASSWORD%
+keytool -list -v -keystore %CLIENT%-trust.p12 -storepass %PASSWORD%
+keytool -list -v -keystore %SERVER%.p12 -storepass %PASSWORD%
+keytool -list -v -keystore %CLIENT%.p12 -storepass %PASSWORD%
