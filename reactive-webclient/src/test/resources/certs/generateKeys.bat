370a54e5cc16773fd5546c10eb5572357669e10a
shi00 <louis2sin@gmail.com>
2022-02-07 10:04:20 +0800

    改进密钥生成脚本

--- reactive-webclient/src/test/resources/certs/generateKeys.bat
+++ reactive-webclient/src/test/resources/certs/generateKeys.bat
@@ -1,21 +1,31 @@
-REM 使用RSA算法生成双向认证证书
+@echo off
+
+::启用utf-8字符集
+CHCP 65001
+
+REM 本脚本功能是使用RSA算法生成双向认证证书
 
 ::删除当前目录下已经生成的证书
-del *.p12 *.cer
+DEL *.p12 *.cer
 
 ::指定生成证书的格式，文件名，主机名
-set STORE_TYPE=PKCS12
-set PASSWORD=password
-set HOST_NAME=localhost
-set SERVER=mockwebserver
-set CLIENT=client
-set VALIDITY=36500
-set KEYSIZE=2048
+SET STORE_TYPE=PKCS12
+SET PASSWORD=password
+SET SERVER=mockwebserver
+SET CLIENT=client
+SET VALIDITY=36500
+SET KEYSIZE=2048
+
+:: 生成服务端证书时，需要在CN中输入客户端访问服务端时使用的域名或ip，例如：https://localhost:port/xxxxx
+:: 生成客户端证书时CN随意
+SET HOST_NAME=localhost
+
+@echo on
 
 ::生成服务端证书
 keytool -genkey -v -alias %SERVER% -keyalg RSA -keystore %SERVER%.p12 -storetype %STORE_TYPE% -validity %VALIDITY% -keysize %KEYSIZE% -storepass %PASSWORD% -dname "CN=%HOST_NAME%,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
 
-keytool -genkey -v -alias %CLIENT% -keyalg RSA -keystore %CLIENT%.p12 -storetype %STORE_TYPE% -validity %VALIDITY% -keysize %KEYSIZE% -storepass %PASSWORD% -dname "CN=%HOST_NAME%,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
+keytool -genkey -v -alias %CLIENT% -keyalg RSA -keystore %CLIENT%.p12 -storetype %STORE_TYPE% -validity %VALIDITY% -keysize %KEYSIZE% -storepass %PASSWORD% -dname "CN=%CLIENT%,OU=DEV,O=Silong,L=Shenzhen,ST=Guangdong,C=CN"
 
 keytool -export -alias %CLIENT% -keystore %CLIENT%.p12 -storetype %STORE_TYPE% -storepass %PASSWORD% -rfc -file %CLIENT%.cer
 
