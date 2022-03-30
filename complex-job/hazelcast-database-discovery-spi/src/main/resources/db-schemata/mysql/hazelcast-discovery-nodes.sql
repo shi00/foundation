2761a7cf87f8f1958e36cce3f439f09777ab5eb8
louis2sin <louis2sin@gmail.com>
2022-03-30 21:06:34 +0800

    升级开源组件版本

--- complex-job/hazelcast-database-discovery-spi/src/main/resources/db-schemata/mysql/hazelcast-discovery-nodes.sql
+++ complex-job/hazelcast-database-discovery-spi/src/main/resources/db-schemata/mysql/hazelcast-discovery-nodes.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS `hazelcast_cluster_nodes`
     `cluster_name`  VARCHAR(32)     NOT NULL, /* 节点所属的集群名 */
     `instance_name` VARCHAR(64), /* 节点所属的实例名 */
     `ip_address`    VARCHAR(50)     NOT NULL, /* 节点IP地址，支持ipv4和ipv6 */
-    `port`          INT(2) UNSIGNED NOT NULL, /* 节点监听端口 */
-    `ip_type`       INT(1) UNSIGNED NOT NULL, /* 节点ip类型，0表示ipv4，其他ipv6 */
+    `port`          INT UNSIGNED NOT NULL, /* 节点监听端口 */
+    `ip_type`       INT UNSIGNED NOT NULL, /* 节点ip类型，0表示ipv4，其他ipv6 */
     `created_time`  TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP, /* 节点记录创建时间 */
     `updated_time`  TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, /* 节点记录更新时间 */
     PRIMARY KEY (`host_name`),
