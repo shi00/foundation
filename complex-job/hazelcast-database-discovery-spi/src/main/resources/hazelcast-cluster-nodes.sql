64b493c662b6e989bd314a1f109bc09591039565
louis2sin <louis2sin@gmail.com>
2022-04-05 13:08:23 +0800

    升级开源组件版本

--- complex-job/hazelcast-database-discovery-spi/src/main/resources/hazelcast-cluster-nodes.sql
+++ complex-job/hazelcast-database-discovery-spi/src/main/resources/hazelcast-cluster-nodes.sql
@@ -1,12 +1,15 @@
+/**
+  创建集群节点记录信息表
+ */
 CREATE TABLE IF NOT EXISTS `hazelcast_cluster_nodes`
 (
-    `host_name`     VARCHAR(32) NOT NULL, /* 节点名 */
-    `cluster_name`  VARCHAR(32) NOT NULL, /* 节点所属的集群名 */
-    `instance_name` VARCHAR(64), /* 节点所属的实例名 */
-    `ip_address`    VARCHAR(50) NOT NULL, /* 节点IP地址，支持ipv4和ipv6 */
-    `port`          INT         NOT NULL, /* 节点监听端口 */
-    `created_time`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP, /* 节点记录创建时间 */
-    `updated_time`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, /* 节点记录更新时间 */
+    `host_name`     VARCHAR(32) NOT NULL COMMENT '节点名',
+    `cluster_name`  VARCHAR(32) NOT NULL COMMENT '节点所属的集群名',
+    `instance_name` VARCHAR(64) comment '节点所属的实例名',
+    `ip_address`    VARCHAR(50) NOT NULL COMMENT '节点IP地址，支持ipv4和ipv6',
+    `port`          INT         NOT NULL COMMENT '节点监听端口',
+    `created_time`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '节点记录创建时间',
+    `updated_time`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '节点记录更新时间',
     PRIMARY KEY (`host_name`, `ip_address`, `port`),
     INDEX HAZELCAST_CLUSTER_NODES_IDX (`cluster_name`, `instance_name`)
 ) ENGINE = INNODB
@@ -14,10 +17,11 @@ CREATE TABLE IF NOT EXISTS `hazelcast_cluster_nodes`
 
 CREATE EVENT IF NOT EXISTS CLEANUP_INACTIVE_HAZELCAST_CLUSTER_NODES_EVENT
     ON SCHEDULE EVERY 1 MONTH
-        STARTS '2022-01-01 00:02:00'
+        STARTS DATE_ADD(DATE_ADD(DATE_SUB(LAST_DAY(NOW()), INTERVAL DAY(LAST_DAY(NOW())) - 1 DAY), INTERVAL 1 MONTH),
+                        INTERVAL 4 HOUR)
     ON COMPLETION PRESERVE ENABLE
-    COMMENT '每月定时清理集群节点表中的去激活节点记录'
+    COMMENT '每月第一天凌晨4时清理集群节点表中的去激活节点记录，所有清理执行时间点前7天的去激活节点记录都会被删除'
     DO
     DELETE
     FROM `hazelcast_cluster_nodes`
-    WHERE DATE_ADD(CURRENT_TIMESTAMP(), interval -(7) day) > `hazelcast_cluster_nodes`.`updated_time`;
+    WHERE DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL -(7) DAY) > `hazelcast_cluster_nodes`.`updated_time`;
