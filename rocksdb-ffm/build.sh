2f6c0d08a87fca7096dadb9ff51a1cdb5fe6f712
louis2sin <louis2sin@gmail.com>
2023-09-16 10:13:51 +0800

    新增rocksdbffm模块，提供rocksdb访问能力

--- rocksdb-ffm/build.sh
+++ rocksdb-ffm/build.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
 ROCKSDB_VER=$1
-cd /opt
-wget --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/v"$ROCKSDB_VER".tar.gz
-tar zxvf v"$ROCKSDB_VER".tar.gz
-cd rocksdb-"$ROCKSDB_VER"
-make clean && make shared_lib
-lld librocksdb.so
+ROCKSDB_DIR=rocksdb-"$ROCKSDB_VER"
+OUTPUT_DIR=$2
+
+cd /opt \
+&& wget --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/v"$ROCKSDB_VER".tar.gz \
+&& tar zxvf v"$ROCKSDB_VER".tar.gz \
+&& cd "$ROCKSDB_DIR" \
+&& make clean && make shared_lib \
+&& ldd librocksdb.so \
+&& jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_DIR" -t com.silong.foundation.rocksdbffm -I /opt/"$ROCKSDB_DIR"/include/rocksdb -I /opt/"$ROCKSDB_DIR"/include/rocksdb/utilities c.h
 
