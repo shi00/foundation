4027594511af44cd09b4950f40a3a5dc25615775
louis2sin <louis2sin@gmail.com>
2023-09-16 19:11:23 +0800

    新增rocksdbffm模块，提供rocksdb访问能力

--- rocksdb-ffm/build.sh
+++ rocksdb-ffm/build.sh
@@ -1,13 +1,15 @@
-#! /bin/sh
+#! /bin/bash
 ROCKSDB_VER=$1
 ROCKSDB_DIR=rocksdb-"$ROCKSDB_VER"
 OUTPUT_DIR=$2
+ROCKSDB_PKG=v"$ROCKSDB_VER".tar.gz
 
-cd /opt \
-&& wget --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/v"$ROCKSDB_VER".tar.gz \
-&& tar zxvf v"$ROCKSDB_VER".tar.gz \
-&& cd "$ROCKSDB_DIR" \
-&& make clean && make shared_lib \
-&& ldd librocksdb.so \
-&& jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_DIR" -t com.silong.foundation.rocksdbffm -I /opt/"$ROCKSDB_DIR"/include/rocksdb -I /opt/"$ROCKSDB_DIR"/include/rocksdb/utilities c.h
+echo "Start downloading $ROCKSDB_PKG ......"
+
+wget -t 3 --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/"$ROCKSDB_PKG"
+tar zxvf "$ROCKSDB_PKG" && cd "$ROCKSDB_DIR"
+source /etc/profile
+make clean && make shared_lib
+ldd librocksdb.so
+jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_DIR" -t com.silong.foundation.rocksdbffm -I /opt/"$ROCKSDB_DIR"/include/rocksdb -I /opt/"$ROCKSDB_DIR"/include/rocksdb/utilities c.h
 
