5533fbb80406ed0495ce0719529702f248db5168
louis2sin <louis2sin@gmail.com>
2023-09-18 07:51:19 +0800

    新增rocksdbffm模块，提供rocksdb访问能力

--- rocksdb-ffm/build.sh
+++ rocksdb-ffm/build.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 ROCKSDB_VER=$1
 ROCKSDB_DIR=rocksdb-"$ROCKSDB_VER"
-OUTPUT_DIR=$2
+OUTPUT_SRC_DIR=$2
 ROCKSDB_PKG=v"$ROCKSDB_VER".tar.gz
-WEB_PORT=$3
+SHARDED_LIB_DIR=$3
 
 source /etc/profile
 
-echo "Start downloading $ROCKSDB_PKG ......"
-
-wget -c -t 3 --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/"$ROCKSDB_PKG"
-tar zxvf "$ROCKSDB_PKG" && cd "$ROCKSDB_DIR"
-make clean && make shared_lib
-ldd librocksdb.so
-jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_DIR" -t com.silong.foundation.rocksdbffm -I /opt/"$ROCKSDB_DIR"/include/rocksdb -I /opt/"$ROCKSDB_DIR"/include/rocksdb/utilities /opt/"$ROCKSDB_DIR"/include/rocksdb/c.h
-#cd .. && mkdir -p output && mv "$OUTPUT_DIR" output && mv "$ROCKSDB_DIR"/librocksdb.so."$ROCKSDB_VER" output/librocksdb.so
-#jwebserver -b 127.0.0.1 -p "$WEB_PORT"
+echo "==================Start downloading $ROCKSDB_PKG=================="
+mkdir -p "$SHARDED_LIB_DIR" \
+&& wget -c -t 3 --no-check-certificate https://github.com/facebook/rocksdb/archive/refs/tags/"$ROCKSDB_PKG" \
+&& tar zxvf "$ROCKSDB_PKG" && cd "$ROCKSDB_DIR" \
+&& make clean && make shared_lib \
+&& ldd librocksdb.so \
+&& mv librocksdb.so."$ROCKSDB_VER" /opt/"$SHARDED_LIB_DIR"/librocksdb.so \
+&& jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_SRC_DIR" -t com.silong.foundation.rocksdbffm -I /opt/"$ROCKSDB_DIR"/include/rocksdb -I /opt/"$ROCKSDB_DIR"/include/rocksdb/utilities /opt/"$ROCKSDB_DIR"/include/rocksdb/c.h
+#cd .. && mkdir -p output && mv "$OUTPUT_SRC_DIR" output && mv "$ROCKSDB_DIR"/librocksdb.so."$ROCKSDB_VER" output/librocksdb.so
+echo "==================Build completed=================="
+exit
