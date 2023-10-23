7f5e86f16c7a23f7342c38c0ab7b13750f3bb7b7
louis2sin <louis2sin@gmail.com>
2023-10-23 20:21:42 +0800

    jwt鉴权工具

---
+++ utilities/jwt-auth/src/main/java/com/silong/foundation/utilities/jwt/Result.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.utilities.jwt;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+/**
+ * 鉴权结果
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2023-10-22 9:36
+ */
+@Data
+@AllArgsConstructor
+@Accessors(fluent = true)
+public class Result {
+
+  /** constants valid */
+  public static final Result VALID = new Result(true, null);
+
+  /** 是否有效 */
+  @Setter private boolean isValid;
+
+  /** 错误原因 */
+  @Setter private String cause;
+}
