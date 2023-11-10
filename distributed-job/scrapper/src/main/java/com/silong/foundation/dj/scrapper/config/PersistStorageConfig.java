2f4afa73586ab46f8ad9adff0aaab0cef63f9116
louis2sin <louis2sin@gmail.com>
2023-11-10 19:27:39 +0800

    新增mixmaster模块功能

---
+++ distributed-job/scrapper/src/main/java/com/silong/foundation/dj/scrapper/config/PersistStorageConfig.java
@@ -0,0 +1,158 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+package com.silong.foundation.dj.scrapper.config;
+
+import static org.rocksdb.StatsLevel.ALL;
+import static org.rocksdb.util.SizeUnit.KB;
+import static org.rocksdb.util.SizeUnit.MB;
+
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.Data;
+import org.rocksdb.StatsLevel;
+
+/**
+ * 持久化存储配置
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2022-04-15 22:12
+ */
+@Data
+public class PersistStorageConfig {
+
+  /** 默认持久化数据保存路径 */
+  public static final String DEFAULT_PERSIST_DATA_PATH =
+      Paths.get(System.getProperty("user.dir")).resolve("data").toFile().getAbsolutePath();
+
+  /** 持久化数据保存路径 */
+  private String persistDataPath = DEFAULT_PERSIST_DATA_PATH;
+
+  /** 列族名列表，不指定则只有default列族 */
+  private List<String> columnFamilyNames = new LinkedList<>();
+
+  /** memtable memory budget. Default: 32MB */
+  private long memtableMemoryBudget = 32 * MB;
+
+  /** 列族写入缓存大小，默认：32MB */
+  private long columnFamilyWriteBufferSize = 32 * MB;
+
+  /** db写入缓存大小，默认：128MB */
+  private long dbWriteBufferSize = 128 * MB;
+
+  /** 最大写缓存数量，默认：4 */
+  private int maxWriteBufferNumber = 4;
+
+  /** 最大后台任务数，默认：6 */
+  private int maxBackgroundJobs = 6;
+
+  /**
+   * Allows OS to incrementally sync files to disk while they are being written, asynchronously, in
+   * the background.Default: 1MB
+   */
+  private long bytesPerSync = MB;
+
+  /** 性能统计配置 */
+  private StatisticsConfig statistics = new StatisticsConfig();
+
+  /** RateLimiter配置 */
+  private RateLimiterConfig rateLimiter = new RateLimiterConfig();
+
+  /** 块表配置 */
+  private BlockBaseTableConfig blockBaseTable = new BlockBaseTableConfig();
+
+  /** 统计配置 */
+  @Data
+  public static class StatisticsConfig {
+    /** 是否可开启存储统计，默认：不开启 */
+    private boolean enable;
+
+    /** 统计级别，默认：ALL */
+    private StatsLevel statsLevel = ALL;
+  }
+
+  /** RateLimiter配置 */
+  @Data
+  public static class RateLimiterConfig {
+    /**
+     * It controls the total write rate of compaction * and flush in bytes per second. Currently,
+     * RocksDB does not enforce * rate limit for anything other than flush and compaction, e.g.
+     * write to * WAL. The default value is 10000000.
+     */
+    private long rateBytesPerSecond = 10_000_000;
+
+    /**
+     * this controls how often tokens are refilled. For * example, * when rate_bytes_per_sec is set
+     * to 10MB/s and refill_period_us is set to * 100ms, then 1MB is refilled every 100ms
+     * internally. Larger value can * lead to burstier writes while smaller value introduces more
+     * CPU * overhead. The default of 10ms should work for most cases.
+     */
+    private long refillPeriodMicros = 10_000;
+
+    /**
+     * RateLimiter accepts high-pri requests and low-pri requests. * A low-pri request is usually
+     * blocked in favor of hi-pri request. * Currently, RocksDB assigns low-pri to request from
+     * compaction and * high-pri to request from flush. Low-pri requests can get blocked if * flush
+     * requests come in continuously. This fairness parameter grants * low-pri requests permission
+     * by fairness chance even though high-pri * requests exist to avoid starvation. * You should be
+     * good by leaving it at default 10.
+     */
+    private int fairness = 10;
+  }
+
+  /** BlockBasedTable配置 */
+  @Data
+  public static class BlockBaseTableConfig {
+
+    /**
+     * Approximate size of user data packed per block. Note that the block size specified here
+     * corresponds to uncompressed data. The actual size of the unit read from disk may be smaller
+     * if compression is enabled. This parameter can be changed dynamically. Default: 16KB
+     */
+    private long blockSize = 16 * KB;
+
+    /** integer representing the version to be used. Default: 5 */
+    private int formatVersion = 5;
+
+    /** 布隆过滤器配置 */
+    private BloomFilterConfig bloomFilter = new BloomFilterConfig();
+
+    /** 缓存配置 */
+    private CacheConfig cache = new CacheConfig();
+
+    /** 布隆过滤器配置 */
+    @Data
+    public static class BloomFilterConfig {
+
+      /** 布隆过滤器bit per key，默认：10.0 */
+      private double bloomFilterBitsPerKey = 10.0;
+    }
+
+    /** 缓存配置 */
+    @Data
+    public static class CacheConfig {
+
+      /** 块缓存容量 */
+      private long blockCacheCapacity = 64 * MB;
+    }
+  }
+}
