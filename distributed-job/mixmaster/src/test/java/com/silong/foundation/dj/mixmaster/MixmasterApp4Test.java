1dd0fb5ba9f6ced917ac52480b4150c2ab8583eb
louis2sin <louis2sin@gmail.com>
2023-11-11 21:59:45 +0800

    新增mixmaster模块功能

---
+++ distributed-job/mixmaster/src/test/java/com/silong/foundation/dj/mixmaster/MixmasterApp4Test.java
@@ -0,0 +1,45 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.dj.mixmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试服务启动入口
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2023-10-26 18:58
+ */
+@SpringBootApplication
+public class MixmasterApp4Test {
+
+  /**
+   * 服务启动入口
+   *
+   * @param args 参数服务
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(MixmasterApp4Test.class, args);
+  }
+}
