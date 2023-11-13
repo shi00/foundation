bbc0e9caf01f7d9a76fbb5e05aaa4a784ddd2207
louis2sin <louis2sin@gmail.com>
2023-11-13 15:49:40 +0800

    新增mixmaster模块

--- distributed-job/mixmaster/src/main/java/com/silong/foundation/dj/mixmaster/Partition2NodesMapping.java
+++ distributed-job/mixmaster/src/main/java/com/silong/foundation/dj/mixmaster/Partition2NodesMapping.java
@@ -21,8 +21,8 @@
 package com.silong.foundation.dj.mixmaster;
 
 import jakarta.annotation.Nullable;
-import java.util.Collection;
 import java.util.Map;
+import java.util.SequencedCollection;
 
 /**
  *
@@ -52,9 +52,9 @@ public interface Partition2NodesMapping<T> {
    * @param neighborhood 各节点邻居关系表，如果此参数不为null，则避免把主备数据映射到互为邻居的节点，确保数据可靠性
    * @return 保存分区的节点列表，其中第一个节点为primary partition节点，后续为backup partition
    */
-  Collection<T> allocatePartition(
+  SequencedCollection<T> allocatePartition(
       int partitionNo,
       int backupNum,
-      Collection<T> clusterNodes,
-      @Nullable Map<T, Collection<T>> neighborhood);
+      SequencedCollection<T> clusterNodes,
+      @Nullable Map<T, SequencedCollection<T>> neighborhood);
 }
