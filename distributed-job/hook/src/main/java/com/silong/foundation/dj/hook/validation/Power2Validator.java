4235e512575dfbf68b49702ae6cdc54ce3a7b064
louis2sin <louis2sin@gmail.com>
2023-11-13 09:21:23 +0800

    新增mixmaster模块

--- distributed-job/hook/src/main/java/com/silong/foundation/dj/hook/validation/Power2Validator.java
+++ distributed-job/hook/src/main/java/com/silong/foundation/dj/hook/validation/Power2Validator.java
@@ -41,7 +41,7 @@ public class Power2Validator implements ConstraintValidator<Power2, Integer> {
   }
 
   /** Returns a power of two size for the given target capacity. */
-  public static int tableSizeFor(int cap) {
+  private static int tableSizeFor(int cap) {
     int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
     return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
   }
