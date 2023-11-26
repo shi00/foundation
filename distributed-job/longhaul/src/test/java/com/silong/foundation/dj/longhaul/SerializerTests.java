04dcae30ca75f7cef07049b89032a2a038d0d140
louis2sin <louis2sin@gmail.com>
2023-11-26 17:30:09 +0800

    重构Rocksdb存储引擎封装

--- distributed-job/longhaul/src/test/java/com/silong/foundation/dj/longhaul/SerializerTests.java
+++ distributed-job/longhaul/src/test/java/com/silong/foundation/dj/longhaul/SerializerTests.java
@@ -21,12 +21,24 @@
 
 package com.silong.foundation.dj.longhaul;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.silong.foundation.dj.longhaul.config.PersistStorageProperties;
 import com.silong.foundation.dj.longhaul.serialize.Int2BytesSerializer;
 import com.silong.foundation.dj.longhaul.serialize.Long2BytesSerializer;
 import com.silong.foundation.dj.longhaul.serialize.String2BytesSerializer;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import net.datafaker.Faker;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,6 +49,10 @@ import org.junit.jupiter.api.Test;
  * @since 2023-11-26 13:48
  */
 public class SerializerTests {
+
+  private RocksDbPersistStorage storage;
+
+  private static final Faker FAKER = new Faker();
 
   @Test
   void test1() {
@@ -67,6 +83,93 @@ public class SerializerTests {
       byte[] bytes = String2BytesSerializer.INSTANCE.to(a);
       String b = String2BytesSerializer.INSTANCE.from(bytes);
       Assertions.assertEquals(a, b);
+    }
+  }
+
+  @Test
+  @SneakyThrows
+  void test4() {
+    var key = new com.silong.foundation.dj.longhaul.SerializerTests.B(1111);
+    var value =
+        new com.silong.foundation.dj.longhaul.SerializerTests.A(RandomStringUtils.random(512));
+    var aClass = com.silong.foundation.dj.longhaul.SerializerTests.A.class;
+    storage.put(key, value);
+    A a = storage.get(key, aClass);
+    Assertions.assertEquals(a, value);
+  }
+
+  @Test
+  @SneakyThrows
+  void test5() {
+    var key = new com.silong.foundation.dj.longhaul.SerializerTests.B(2312312);
+    var value =
+        new com.silong.foundation.dj.longhaul.SerializerTests.A(RandomStringUtils.random(1028));
+    var aClass = com.silong.foundation.dj.longhaul.SerializerTests.A.class;
+    storage.put(key, value);
+    A a = storage.get(key, aClass);
+    Assertions.assertEquals(a, value);
+
+    storage.remove(key);
+    a = storage.get(key, aClass);
+    Assertions.assertNull(a);
+  }
+
+  @BeforeEach
+  void init() {
+    PersistStorageProperties config = new PersistStorageProperties();
+    List<String> cls = new ArrayList<>(1000);
+    for (int i = 0; i < 1000; i++) {
+      cls.add(FAKER.animal().name());
+    }
+    config.setColumnFamilyNames(cls);
+    config.setPersistDataPath(String.format("./target/%s-data/", FAKER.ancient().titan()));
+    storage = new RocksDbPersistStorage(config, false);
+  }
+
+  @AfterEach
+  void cleanup() {
+    if (storage != null) {
+      storage.close();
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  private static class B implements TypeConverter<B> {
+    private long aLong;
+
+    @Override
+    public byte[] serialize() {
+      return Long2BytesSerializer.INSTANCE.to(aLong);
+    }
+
+    @Override
+    public B deserialize(byte[] bytes) {
+      if (bytes == null) {
+        return null;
+      }
+      return new B(Long2BytesSerializer.INSTANCE.from(bytes));
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  private static class A implements TypeConverter<A> {
+    private String str;
+
+    @Override
+    public byte[] serialize() {
+      return str.getBytes(UTF_8);
+    }
+
+    @Override
+    public A deserialize(byte[] bytes) {
+      if (bytes == null) {
+        return null;
+      }
+      return new A(new String(bytes, UTF_8));
     }
   }
 }
