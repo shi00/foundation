c10ae8bca8e2805f47d8473b36ea37c1ab286604
louis2sin <louis2sin@gmail.com>
2023-11-28 15:09:35 +0800

    重构Rocksdb存储引擎封装

--- spring-boot-starter-webflux-simpleAuth/src/main/java/com/silong/foundation/springboot/starter/simpleauth/configure/SecurityAutoConfiguration.java
+++ spring-boot-starter-webflux-simpleAuth/src/main/java/com/silong/foundation/springboot/starter/simpleauth/configure/SecurityAutoConfiguration.java
@@ -30,6 +30,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -49,6 +50,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
  */
 @Slf4j
 @EnableWebFlux
+@Configuration
 @EnableConfigurationProperties(SimpleAuthProperties.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 public class SecurityAutoConfiguration {
