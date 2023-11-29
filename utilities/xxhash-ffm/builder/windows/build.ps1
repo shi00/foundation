19fba62d1daced2edc9d5229edef310f00343efa
louis2sin <louis2sin@gmail.com>
2023-11-29 15:42:39 +0800

    xxhash windows平台构建脚本修改为powershell实现

---
+++ utilities/xxhash-ffm/builder/windows/build.ps1
@@ -0,0 +1,41 @@
+﻿param([string]$srcDir,[string]$dllDir,[string]$libName)
+Write-Output "srcDir: $srcDir" 
+Write-Output "dllDir: $dllDir"
+Write-Output "libName: $libName"
+
+$dllFile_x64="C:\vcpkg\installed\x64-windows\bin\xxhash.dll"
+$dllFile_x86="C:\vcpkg\installed\x86-windows\bin\xxhash.dll"
+$includeDir=""
+
+echo "Start to build xxHash ...... "
+.\vcpkg install xxhash
+
+$is_dll_x86_exist=[System.IO.File]::Exists($dllFile_x86)
+$is_dll_x64_exist=[System.IO.File]::Exists($dllFile_x64)
+
+if($is_dll_x64_exist){
+Move-Item $dllFile_x64 c:\%dllDir%\
+Rename-Item c:\$dllDir\xxhash.dll $libName.dll
+$includeDir="C:\vcpkg\installed\x64-windows\include"
+echo "xxhash:x64-windows has been successfully built."
+} elseif($is_dll_x86_exist){
+Move-Item $dllFile_x86 c:\%dllDir%\
+Rename-Item c:\$dllDir\xxhash.dll $libName.dll
+$includeDir="C:\vcpkg\installed\x86-windows\include"
+echo "xxhash:x86-windows has been successfully built."
+}else{
+echo "Failed to build xxhash."
+Exit 1
+}
+
+
+echo "================== Start generate source code for xxHash =================="
+.\jextract --source --header-class-name xxHash --output c:\%srcDir% -t com.silong.foundation.utilities.xxhash.generated -I %includeDir% %includeDir%\xxhash.h
+$is_src_dir=[System.IO.Directory]::Exists($srcDir)
+if(!$is_src_dir){
+echo "Failed to generate code by jextract"
+Exit 1
+}
+
+echo "================== Build completed =================="
+Start-Sleep -Seconds 5
