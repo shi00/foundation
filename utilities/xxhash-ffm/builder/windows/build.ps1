bbcadcbc01051051bc11a77082953dd53a044429
louis2sin <louis2sin@gmail.com>
2023-11-30 15:14:06 +0800

    重构代码

--- utilities/xxhash-ffm/builder/windows/build.ps1
+++ utilities/xxhash-ffm/builder/windows/build.ps1
@@ -14,13 +14,13 @@ $is_dll_x86_exist=[System.IO.File]::Exists($dllFile_x86)
 $is_dll_x64_exist=[System.IO.File]::Exists($dllFile_x64)
 
 if($is_dll_x64_exist){
-Move-Item $dllFile_x64 c:\%dllDir%\
-Rename-Item c:\$dllDir\xxhash.dll $libName.dll
+Move-Item -Path $dllFile_x64 -Destination c:\$dllDir\
+Rename-Item -Path c:\$dllDir\xxhash.dll -NewName $libName.dll
 $includeDir="C:\vcpkg\installed\x64-windows\include"
 echo "xxhash:x64-windows has been successfully built."
 } elseif($is_dll_x86_exist){
-Move-Item $dllFile_x86 c:\%dllDir%\
-Rename-Item c:\$dllDir\xxhash.dll $libName.dll
+Move-Item -Path $dllFile_x86 -Destination c:\$dllDir\
+Rename-Item -Path c:\$dllDir\xxhash.dll -NewName $libName.dll
 $includeDir="C:\vcpkg\installed\x86-windows\include"
 echo "xxhash:x86-windows has been successfully built."
 }else{
@@ -28,9 +28,8 @@ echo "Failed to build xxhash."
 Exit 1
 }
 
-
 echo "================== Start generate source code for xxHash =================="
-.\jextract --source --header-class-name xxHash --output c:\%srcDir% -t com.silong.foundation.utilities.xxhash.generated -I %includeDir% %includeDir%\xxhash.h
+jextract --source --header-class-name xxHash --output c:\$srcDir -t com.silong.foundation.utilities.xxhash.generated -I $includeDir $includeDir\xxhash.h
 $is_src_dir=[System.IO.Directory]::Exists($srcDir)
 if(!$is_src_dir){
 echo "Failed to generate code by jextract"
