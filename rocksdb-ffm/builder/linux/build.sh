4c52de6101225bfbb29eee504bb5200c028f29e4
louis2sin <louis2sin@gmail.com>
2023-12-01 23:05:08 +0800

    采用vcpkg构建rocksdb linux

--- rocksdb-ffm/builder/linux/build.sh
+++ rocksdb-ffm/builder/linux/build.sh
@@ -1,35 +1,30 @@
 #! /bin/sh
 OUTPUT_SRC_DIR=$1
 SHARDED_LIB_DIR=$2
-OS_NAME=$(echo $(uname -s) | tr '[A-Z]' '[a-z]')   # 获取操作系统名
-OS_ARCH=$(uname -p)                  # 获取操作系统架构
 SHARDED_LIB_NAME=$3".so"
 
-source /etc/profile
-
 echo "$OS_NAME" "$OS_ARCH" "$OUTPUT_SRC_DIR" "$SHARDED_LIB_DIR"
-echo "================== Start building xxHash =================="
-cd vcpkg
+echo "================== Start building rocksdb =================="
+
 mkdir custom-triplets
 cp triplets/x64-linux.cmake custom-triplets/x64-linux-dynamic.cmake
 sed -i 's/set(VCPKG_LIBRARY_LINKAGE static)/set(VCPKG_LIBRARY_LINKAGE dynamic)/g' custom-triplets/x64-linux-dynamic.cmake
-./vcpkg install xxhash:x64-linux-dynamic --overlay-triplets=custom-triplets
+./vcpkg install rocksdb:x64-linux-dynamic --overlay-triplets=custom-triplets
 
 #判断文件是否存在
 if [ ! -f installed/x64-linux-dynamic/lib/"$SHARDED_LIB_NAME" ]; then
-    echo "Failed to build xxhash."
+    echo "Failed to build rocksdb."
     exit 1
 fi
 
-mkdir -p /opt/"$SHARDED_LIB_DIR"/native/"$OS_NAME"/"$OS_ARCH"
-cp installed/x64-linux-dynamic/lib/"$SHARDED_LIB_NAME" /opt/"$SHARDED_LIB_DIR"/native/"$OS_NAME"/"$OS_ARCH"/"$SHARDED_LIB_NAME"
+cp installed/x64-linux-dynamic/lib/"$SHARDED_LIB_NAME" /opt/"$SHARDED_LIB_DIR"
 
-echo "================== Start generate source code for xxHash =================="
-jextract --source --header-class-name xxHash --output /opt/"$OUTPUT_SRC_DIR" -t com.silong.foundation.utilities.xxhash.generated -I /opt/vcpkg/installed/x64-linux-dynamic/include /opt/vcpkg/installed/x64-linux-dynamic/include/xxhash.h
+echo "================== Start generate source code for rocksdb =================="
+jextract --source --header-class-name RocksDB --output /opt/"$OUTPUT_SRC_DIR" -t com.silong.foundation.rocksdb.generated -I /vcpkg/installed/x64-linux-dynamic/include/rocksdb /vcpkg/installed/x64-linux-dynamic/include/rocksdb/c.h
 if [ ! -d "/opt/$OUTPUT_SRC_DIR" ];then
   echo "Failed to generate code by jextract"
   exit 1
 fi
 
 echo "================== Build completed =================="
-sleep 10s
+sleep 5s
