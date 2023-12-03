1b68988685ff370b5492bb12f7b658ff5bd96785
louis2sin <louis2sin@gmail.com>
2023-12-03 22:04:12 +0800

    采用vcpkg构建rocksdb

---
+++ rocksdb-ffm/src/main/java/com/silong/foundation/rocksdbffm/RocksDb.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.rocksdbffm;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * RocksDB提供的API接口封装
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2023-09-10 14:44
+ */
+public interface RocksDb extends AutoCloseable {
+
+  /** 默认列族名称 */
+  String DEFAULT_COLUMN_FAMILY_NAME = "default";
+
+  /** 默认列族键 */
+  byte[] DEFAULT_COLUMN_FAMILY_KEY = DEFAULT_COLUMN_FAMILY_NAME.getBytes(UTF_8);
+
+  /**
+   * 获取实例
+   *
+   * @param config 配置
+   * @return 实例
+   */
+  static RocksDb getInstance(RocksDbConfig config) {
+    return new RocksDbImpl(config);
+  }
+}
