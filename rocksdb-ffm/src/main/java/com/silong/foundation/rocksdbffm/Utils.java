64e3f87ea7d74eb76354cefa2ee0f2b488a18af4
louis2sin <louis2sin@gmail.com>
2023-12-07 14:56:45 +0800

    采用vcpkg构建rocksdb，并且使用FFM使用rocksdb

--- rocksdb-ffm/src/main/java/com/silong/foundation/rocksdbffm/Utils.java
+++ rocksdb-ffm/src/main/java/com/silong/foundation/rocksdbffm/Utils.java
@@ -87,6 +87,12 @@ class Utils {
     return str == null || str.isEmpty();
   }
 
+  public static void validateColumnFamilyName(String columnFamilyName) {
+    if (isEmpty(columnFamilyName)) {
+      throw new IllegalArgumentException("columnFamilyName must not be null or empty.");
+    }
+  }
+
   public static void validateKey(byte[] key) {
     validateByteArrays(key, "key must not be null or empty.");
   }
