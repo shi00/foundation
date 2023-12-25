ae7b03e270429ed223b734c34a71d2bfffd0c698
louis sin <louis2sin@gmail.com>
2023-12-25 20:54:45 +0800

    采用vcpkg构建rocksdb，并且使用FFM使用rocksdb

---
+++ rocksdb-ffm/src/main/java/com/silong/foundation/rocksdbffm/enu/Usage.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.rocksdbffm.enu;
+
+/**
+ * 使用方式，rocksdb会依据数据规模优化配置参数
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2023-12-25 17:03
+ */
+public enum Usage {
+  // 小数据量，1GB数据量以内(测试用)
+  SMALL,
+
+  // 普通点查场景
+  POINT_LOOKUP
+}
