550990582b8fea9eb51cebb397659a8678d4108e
louis sin <louis2sin@gmail.com>
2024-01-16 16:41:06 +0800

    使用静态内部类

--- duuid/duuid-server/src/main/java/com/silong/foundation/duuid/server/configure/properties/WorkerIdProviderProperties.java
+++ duuid/duuid-server/src/main/java/com/silong/foundation/duuid/server/configure/properties/WorkerIdProviderProperties.java
@@ -18,16 +18,15 @@
  */
 package com.silong.foundation.duuid.server.configure.properties;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.validation.annotation.Validated;
-
-import java.util.List;
-
-import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 /**
  * WorkerIdProvider配置
@@ -40,85 +39,78 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
 @Validated
 @ConfigurationProperties(prefix = "duuid.worker-id-provider")
 @SuppressFBWarnings(
-        value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
-        justification = "只读初始配置")
+    value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+    justification = "只读初始配置")
 public class WorkerIdProviderProperties {
-    /** WorkerId Provider类型，etcdv3或者mysql */
-    @NotEmpty
-    private String type;
+  /** WorkerId Provider类型，etcdv3或者mysql */
+  @NotEmpty private String type;
+
+  /** etcd配置 */
+  @NestedConfigurationProperty private EtcdProperties etcdv3 = new EtcdProperties();
+
+  /** mysql配置 */
+  @NestedConfigurationProperty private MysqlProperties mysql = new MysqlProperties();
+
+  /**
+   * 基于etcdv3的workerId分配器配置
+   *
+   * @author louis sin
+   * @version 1.0.0
+   * @since 2022-01-11 22:42
+   */
+  @Data
+  @Validated
+  @SuppressFBWarnings(
+      value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+      justification = "只读初始配置")
+  public static class EtcdProperties {
+
+    /** 服务器地址 */
+    private List<String> serverAddresses;
 
     /**
-     * etcd配置
+     * Trusted certificates for verifying the remote endpoint's certificate. The file should contain
+     * an X.509 certificate collection in PEM format.
      */
-    @NestedConfigurationProperty
-    private EtcdProperties etcdv3 = new EtcdProperties();
+    private String trustCertCollectionFile = EMPTY;
 
-    /**
-     * mysql配置
-     */
-    @NestedConfigurationProperty
-    private MysqlProperties mysql = new MysqlProperties();
+    /** a PKCS#8 private key file in PEM format */
+    private String keyFile = EMPTY;
 
-    /**
-     * 基于etcdv3的workerId分配器配置
-     *
-     * @author louis sin
-     * @version 1.0.0
-     * @since 2022-01-11 22:42
-     */
-    @Data
-    @Validated
-    @SuppressFBWarnings(
-            value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
-            justification = "只读初始配置")
-    public static class EtcdProperties {
+    /** an X.509 certificate chain file in PEM format */
+    private String keyCertChainFile = EMPTY;
 
-        /** 服务器地址 */
-        private List<String> serverAddresses;
+    /** 用户名 */
+    private String userName = EMPTY;
 
-        /**
-         * Trusted certificates for verifying the remote endpoint's certificate. The file should contain
-         * an X.509 certificate collection in PEM format.
-         */
-        private String trustCertCollectionFile = EMPTY;
+    /** 密码 */
+    private String password = EMPTY;
+  }
 
-        /** a PKCS#8 private key file in PEM format */
-        private String keyFile = EMPTY;
+  /**
+   * 基于mysql的workerId分配器配置
+   *
+   * @author louis sin
+   * @version 1.0.0
+   * @since 2022-01-11 22:42
+   */
+  @Data
+  @Validated
+  @SuppressFBWarnings(
+      value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+      justification = "只读初始配置")
+  public static class MysqlProperties {
 
-        /** an X.509 certificate chain file in PEM format */
-        private String keyCertChainFile = EMPTY;
+    /** jdbc url */
+    private String jdbcUrl = EMPTY;
 
-        /** 用户名 */
-        private String userName = EMPTY;
+    /** jdbc驱动 */
+    private String jdbcDriver = EMPTY;
 
-        /** 密码 */
-        private String password = EMPTY;
-    }
+    /** 用户名 */
+    private String userName = EMPTY;
 
-    /**
-     * 基于mysql的workerId分配器配置
-     *
-     * @author louis sin
-     * @version 1.0.0
-     * @since 2022-01-11 22:42
-     */
-    @Data
-    @Validated
-    @SuppressFBWarnings(
-            value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
-            justification = "只读初始配置")
-    public class MysqlProperties {
-
-        /** jdbc url */
-        private String jdbcUrl = EMPTY;
-
-        /** jdbc驱动 */
-        private String jdbcDriver = EMPTY;
-
-        /** 用户名 */
-        private String userName = EMPTY;
-
-        /** 密码 */
-        private String password = EMPTY;
-    }
+    /** 密码 */
+    private String password = EMPTY;
+  }
 }
