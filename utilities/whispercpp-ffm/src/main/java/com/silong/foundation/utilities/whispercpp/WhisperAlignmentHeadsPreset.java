c33a064834f9b927a2aab4d0b85894ea9dccee5c
louis sin <louis2sin@gmail.com>
2024-04-22 20:29:14 +0800

    添加whispercpp支持

---
+++ utilities/whispercpp-ffm/src/main/java/com/silong/foundation/utilities/whispercpp/WhisperAlignmentHeadsPreset.java
@@ -0,0 +1,46 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.utilities.whispercpp;
+
+/**
+ * Whisper 头对齐预设值
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2024-04-22 16:27
+ */
+public enum WhisperAlignmentHeadsPreset {
+  WHISPER_AHEADS_NONE,
+  WHISPER_AHEADS_N_TOP_MOST, // All heads from the N-top-most text-layers
+  WHISPER_AHEADS_CUSTOM,
+  WHISPER_AHEADS_TINY_EN,
+  WHISPER_AHEADS_TINY,
+  WHISPER_AHEADS_BASE_EN,
+  WHISPER_AHEADS_BASE,
+  WHISPER_AHEADS_SMALL_EN,
+  WHISPER_AHEADS_SMALL,
+  WHISPER_AHEADS_MEDIUM_EN,
+  WHISPER_AHEADS_MEDIUM,
+  WHISPER_AHEADS_LARGE_V1,
+  WHISPER_AHEADS_LARGE_V2,
+  WHISPER_AHEADS_LARGE_V3,
+}
