9e9094f10845e0b44be2353f14675b395d4fabe5
louis sin <louis2sin@gmail.com>
2024-05-06 14:18:53 +0800

    添加语音助手服务

---
+++ spring-boot-starter-webflux-tokenAuth/src/main/java/com/silong/foundation/springboot/starter/tokenauth/configure/config/SimpleAuthProperties.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.silong.foundation.springboot.starter.tokenauth.configure.config;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * 服务配置
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2022-01-03 11:31
+ */
+@Data
+@Validated
+@ConfigurationProperties(prefix = "token-auth")
+@SuppressFBWarnings(
+    value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+    justification = "Read-only initial configuration")
+public class SimpleAuthProperties {
+
+  /** token超时时长，默认：3600秒，如果为0表示永不超时 */
+  @PositiveOrZero private int tokenTimeout = (int) TimeUnit.HOURS.toSeconds(1);
+
+  /** 使用HmacSha256签名使用的密钥 */
+  @NotEmpty private String workKey;
+
+  /** 无需鉴权请求路径名单 */
+  @Valid @NotEmpty private Set<@NotEmpty String> whiteList;
+
+  /** 需鉴权请求路径名单 */
+  @Valid @NotEmpty private Set<@NotEmpty String> authList;
+
+  /** 用户到角色映射 */
+  @Valid @NotEmpty
+  private Map<@NotEmpty String, @Valid @NotEmpty Set<@NotEmpty String>> userRolesMappings;
+
+  /** 角色到请求路径映射 */
+  @Valid @NotEmpty
+  private Map<@NotEmpty String, @NotEmpty @Valid Set<@NotEmpty String>> rolePathsMappings;
+}
