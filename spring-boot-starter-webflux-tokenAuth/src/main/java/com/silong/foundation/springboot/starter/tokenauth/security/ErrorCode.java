960138971141ddf4c3423a149f8deaf0a8afcf65
louis sin <louis2sin@gmail.com>
2024-05-07 09:39:46 +0800

    添加语音助手服务

---
+++ spring-boot-starter-webflux-tokenAuth/src/main/java/com/silong/foundation/springboot/starter/tokenauth/security/ErrorCode.java
@@ -0,0 +1,46 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.foundation.springboot.starter.tokenauth.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 错误码
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2024-05-07 9:33
+ */
+@Getter
+@AllArgsConstructor
+enum ErrorCode {
+  /** 授权失败 */
+  UNAUTHORIZED("%s.003"),
+  /** 认证失败 */
+  UNAUTHENTICATED("%s.002"),
+  /** 禁止访问 */
+  FORBIDDEN("%s.001");
+
+  /** 错误码格式 */
+  private final String format;
+}
