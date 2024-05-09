e2e023db5ab55966496ee72106043da95dd43ee4
louis sin <louis2sin@gmail.com>
2024-05-09 14:12:35 +0800

    添加语音助手服务

--- utilities/whispercpp-ffm/builder/linux/build.sh
+++ utilities/whispercpp-ffm/builder/linux/build.sh
@@ -21,9 +21,11 @@ if echo "$BUILD_PARAMS" | grep -iqF "openvino"; then
  echo "Start downloading OpenVINO......"
  openvino_file=${BUILD_PARAMS##*/} #substring 最后一个/后的字符串
  openvino_dir=${openvino_file%.*}  #substring 最后一个.前的字符串
+ openvino_path="/opt/whisper.cpp/$openvino_dir/runtime/cmake/"
  echo "openvino_file=$openvino_file"
  echo "openvino_dir=$openvino_dir"
  echo "openvino_url=$BUILD_PARAMS"
+ echo "openvino_path=$openvino_path"
  wget -t 3 -c --no-check-certificate "$BUILD_PARAMS"  # 下载openvino
  if [ ! -f "./$openvino_file" ];then
    echo "Failed to download $openvino_file."
@@ -34,30 +36,43 @@ if echo "$BUILD_PARAMS" | grep -iqF "openvino"; then
  tar zxvf "$openvino_file"
  rm -rf "$openvino_file"
 
+ cd "$openvino_dir"
+ yes | source ./install_dependencies/install_openvino_dependencies.sh
+ cd ".."
+
  cd "models"
  python3 -m venv openvino_conv_env && source "openvino_conv_env/bin/activate" && python -m pip install --upgrade pip  && pip install -r requirements-openvino.txt
  source "/opt/whisper.cpp/$openvino_dir/setupvars.sh"
 
  # 开始构建
  cd ".."
+ sed -Ei "s|# Add path to modules|list(APPEND CMAKE_PREFIX_PATH \"$openvino_path\")|" CMakeLists.txt
 
  cmake -B build -DWHISPER_OPENVINO=1
  cmake --build build -j --config Release
+
+ #判断文件是否存在
+ if [ ! -f "./build/$SHARDED_LIB_NAME" ]; then
+     echo "Failed to build whispercpp."
+     exit 1
+ fi
+
+ cp "./build/$SHARDED_LIB_NAME" /opt/"$SHARDED_LIB_DIR"/"$SHARDED_LIB_NAME"
 
 elif echo "$BUILD_PARAMS" | grep -iqF "cuda"; then
   echo "CUDA is not supported now."
   exit 1
 else
   make "$SHARDED_LIB_NAME" && make stream
-fi
 
-#判断文件是否存在
-if [ ! -f "$SHARDED_LIB_NAME" ]; then
-    echo "Failed to build whispercpp."
-    exit 1
-fi
+  #判断文件是否存在
+  if [ ! -f "$SHARDED_LIB_NAME" ]; then
+      echo "Failed to build whispercpp."
+      exit 1
+  fi
 
-cp "$SHARDED_LIB_NAME" /opt/"$SHARDED_LIB_DIR"/"$SHARDED_LIB_NAME"
+  cp "$SHARDED_LIB_NAME" /opt/"$SHARDED_LIB_DIR"/"$SHARDED_LIB_NAME"
+fi
 
 echo "================== Start generate source code for whispercpp =================="
 jextract --header-class-name "$HEADER_CLASS_NAME" --output /opt/"$OUTPUT_SRC_DIR" --target-package "$SOURCECODE_PACKAGE" --include-dir . whisper.h
