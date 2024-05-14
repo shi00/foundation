aae7271d0c1d702741e7b7debf92ed4c0190a4f0
louis sin <louis2sin@gmail.com>
2024-05-14 20:57:16 +0800

    添加语音助手服务

--- spring-boot-starter-webflux-jwt/src/main/java/com/silong/foundation/springboot/starter/jwt/security/SimpleServerAuthenticationConverter.java
+++ spring-boot-starter-webflux-jwt/src/main/java/com/silong/foundation/springboot/starter/jwt/security/SimpleServerAuthenticationConverter.java
@@ -16,32 +16,19 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.silong.foundation.springboot.starter.tokenauth.security;
+package com.silong.foundation.springboot.starter.jwt.security;
 
-import static com.silong.foundation.springboot.starter.tokenauth.misc.Constants.*;
-import static java.util.Collections.singletonList;
+import static com.silong.foundation.springboot.starter.jwt.common.Constants.*;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 import static org.springframework.util.StringUtils.hasLength;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.silong.foundation.springboot.starter.tokenauth.configure.config.SimpleAuthProperties;
-import com.silong.foundation.springboot.starter.tokenauth.exception.AccessForbiddenException;
-import com.silong.foundation.springboot.starter.tokenauth.exception.AccessTokenNotFoundException;
-import com.silong.foundation.springboot.starter.tokenauth.exception.IdentityNotFoundException;
-import com.silong.foundation.springboot.starter.tokenauth.exception.IllegalAccessTokenException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.silong.foundation.springboot.starter.jwt.configure.config.JWTAuthProperties;
+import com.silong.foundation.springboot.starter.jwt.exception.AccessForbiddenException;
+import com.silong.foundation.springboot.starter.jwt.exception.AccessTokenNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
@@ -58,13 +45,9 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class SimpleServerAuthenticationConverter implements ServerAuthenticationConverter {
 
-  /** 百名单授权 */
-  private static final Authentication GEUST =
-      new SimpleAuthenticationToken(null, singletonList(new SimpleGrantedAuthority("guest")), true);
-
-  private final JWTVerifier verifier;
-
-  private final Map<String, List<SimpleGrantedAuthority>> cache;
+  /** 用户访问白名单列表内的url时使用GUEST鉴权进行访问，无需鉴权 */
+  private static final Authentication GUEST =
+      new SimpleTokenAuthentication(createAuthorityList("guest"), true, null);
 
   private final ServerWebExchangeMatcher noAuthServerWebExchangeMatcher;
 
@@ -74,25 +57,12 @@ public class SimpleServerAuthenticationConverter implements ServerAuthentication
    * 构造方法
    *
    * @param properties 服务配置
-   * @param algorithm token签名算法
-   * @param appName 应用名
    */
-  public SimpleServerAuthenticationConverter(
-      SimpleAuthProperties properties, Algorithm algorithm, String appName) {
-    Map<String, List<SimpleGrantedAuthority>> map = new HashMap<>();
-    properties
-        .getUserRolesMappings()
-        .forEach(
-            (key, value) ->
-                map.put(
-                    key,
-                    value.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())));
-    this.cache = Collections.unmodifiableMap(map);
+  public SimpleServerAuthenticationConverter(JWTAuthProperties properties) {
     this.authServerWebExchangeMatcher =
         ServerWebExchangeMatchers.pathMatchers(properties.getAuthList().toArray(new String[0]));
     this.noAuthServerWebExchangeMatcher =
         ServerWebExchangeMatchers.pathMatchers(properties.getWhiteList().toArray(new String[0]));
-    this.verifier = JWT.require(algorithm).withIssuer(appName).build();
   }
 
   @Override
@@ -100,12 +70,12 @@ public class SimpleServerAuthenticationConverter implements ServerAuthentication
     return noAuthServerWebExchangeMatcher
         .matches(exchange)
         .filter(ServerWebExchangeMatcher.MatchResult::isMatch)
-        .map(matchResult -> GEUST) // 匹配白名单，则以guest身份访问
+        .map(matchResult -> GUEST) // 匹配白名单，则以guest身份访问
         .switchIfEmpty(
             authServerWebExchangeMatcher
                 .matches(exchange)
                 .filter(ServerWebExchangeMatcher.MatchResult::isMatch)
-                .map(matchResult -> getNeedAuthentication(exchange))
+                .map(matchResult -> extract(exchange))
                 .switchIfEmpty(
                     Mono.defer(
                         () ->
@@ -115,34 +85,18 @@ public class SimpleServerAuthenticationConverter implements ServerAuthentication
                                         FORBIDDEN.getReasonPhrase()))))); // 白名单和鉴权名单外的访问全部禁止
   }
 
-  private Authentication getNeedAuthentication(ServerWebExchange exchange) {
+  /**
+   * 从请求中抽取鉴权信息
+   *
+   * @param exchange 请求
+   * @return 鉴权信息
+   */
+  private Authentication extract(ServerWebExchange exchange) {
     HttpHeaders httpHeaders = exchange.getRequest().getHeaders();
     String at = httpHeaders.getFirst(ACCESS_TOKEN);
     if (!hasLength(at)) {
       throw new AccessTokenNotFoundException("AccessToken is not exist in request headers.");
     }
-
-    DecodedJWT decodedJWT;
-    try {
-      decodedJWT = verifier.verify(at);
-    } catch (JWTVerificationException e) {
-      log.error("Failed to verify token.", e);
-      throw new IllegalAccessTokenException(e.getMessage());
-    }
-
-    String identity = decodedJWT.getClaim(IDENTITY).asString();
-    if (!hasLength(identity)) {
-      log.error("{} could not be found in the access token.", IDENTITY);
-      throw new IdentityNotFoundException(IDENTITY + " could not be found in the access token.");
-    }
-
-    List<SimpleGrantedAuthority> grantedAuthorities = cache.get(identity);
-    if (grantedAuthorities == null || grantedAuthorities.isEmpty()) {
-      String msg =
-          String.format("No authorization could be found for the %s:%s.", IDENTITY, identity);
-      log.error(msg);
-      throw new IllegalAccessTokenException(msg);
-    }
-    return new SimpleAuthenticationToken(decodedJWT, grantedAuthorities, false);
+    return new SimpleTokenAuthentication(null, false, at);
   }
 }
