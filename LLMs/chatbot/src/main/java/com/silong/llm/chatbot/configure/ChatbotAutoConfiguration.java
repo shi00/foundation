36bc6cd2be7aabe30db05b85333c8dfb75f581b1
louis sin <louis2sin@gmail.com>
2025-05-22 22:45:01 +0800

    chatbot首次合入

--- LLMs/chatbot/src/main/java/com/silong/llm/chatbot/configure/ChatbotAutoConfiguration.java
+++ LLMs/chatbot/src/main/java/com/silong/llm/chatbot/configure/ChatbotAutoConfiguration.java
@@ -34,11 +34,13 @@ import java.net.InetSocketAddress;
 import javax.net.ssl.SSLException;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.model.azure.openai.autoconfigure.AzureOpenAIClientBuilderCustomizer;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -73,10 +75,12 @@ public class ChatbotAutoConfiguration {
   }
 
   @Bean
-  ChatClient chatClient(ChatClient.Builder builder) {
+  ChatClient chatClient(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider) {
     return builder
+        .defaultToolCallbacks(toolCallbackProvider)
         .defaultSystem(properties.getSystemMessage())
         .defaultAdvisors(
+            new SimpleLoggerAdvisor(),
             MessageChatMemoryAdvisor.builder(chatMemory())
                 .scheduler(Schedulers.boundedElastic())
                 .build())
