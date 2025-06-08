45bf03683a4c153496a3abc58533a7dc04be81ab
louis sin <louis2sin@gmail.com>
2025-06-08 16:29:26 +0800

    chatbot首次合入

---
+++ LLMs/chatbot-desktop/src/main/java/com/silong/llm/chatbot/desktop/utils/ResourceLoader.java
@@ -0,0 +1,55 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.llm.chatbot.desktop.utils;
+
+import java.io.InputStream;
+import java.net.URL;
+import lombok.NonNull;
+
+/**
+ * 资源加载工具
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2025-05-19 20:19
+ */
+public interface ResourceLoader {
+  /**
+   * 加载classpath内的资源
+   *
+   * @param path 资源在classpath路径
+   * @return 资源URL
+   */
+  static URL loadURL(@NonNull String path) {
+    return ResourceLoader.class.getResource(path);
+  }
+
+  /**
+   * 加载classpath内的资源
+   *
+   * @param path 资源在classpath路径
+   * @return 资源流
+   */
+  static InputStream loadStream(@NonNull String path) {
+    return ResourceLoader.class.getResourceAsStream(path);
+  }
+}
