7ed6cd5f1627dcb8e1c4b7e7826d886444d04a1f
louis sin <louis2sin@gmail.com>
2025-06-15 18:25:32 +0800

    chatbot首次合入

--- LLMs/chatbot/src/test/java/com/silong/llm/chatbot/ChatbotApplicationTests.java
+++ LLMs/chatbot/src/test/java/com/silong/llm/chatbot/ChatbotApplicationTests.java
@@ -21,7 +21,131 @@
 
 package com.silong.llm.chatbot;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.util.StringUtils.hasLength;
 
-@SpringBootTest
-class ChatbotApplicationTests {}
+import com.silong.foundation.springboot.starter.jwt.common.Credentials;
+import com.silong.foundation.springboot.starter.jwt.common.TokenBody;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.filter.EqualsFilter;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureWebTestClient
+public class ChatbotApplicationTests {
+
+  @Container
+  private static final GenericContainer<?> REDIS_CONTAINER =
+      new GenericContainer<>("redis:6.2.6")
+          .withExposedPorts(6379)
+          .waitingFor(Wait.forListeningPort())
+          .withStartupTimeout(Duration.ofMinutes(1));
+
+  private static final String DOMAIN = "test.com";
+  private static final String ADMIN_PASSWORD = "Secret@123";
+
+  @Container
+  private static GenericContainer<?> LDAP_CONTAINER =
+      new GenericContainer<>("osixia/openldap:1.5.0")
+          .withEnv("LDAP_DOMAIN", DOMAIN)
+          .withEnv("LDAP_ADMIN_PASSWORD", ADMIN_PASSWORD)
+          .withExposedPorts(389)
+          .withCopyFileToContainer(
+              MountableFile.forClasspathResource("ldap/users.ldif"), // LDIF 文件路径
+              "/tmp/users.ldif");
+
+  @DynamicPropertySource
+  static void redisProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+    registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+    registry.add(
+        "spring.ldap.urls",
+        () ->
+            new String[] {
+              String.format(
+                  "ldap://%s:%d", LDAP_CONTAINER.getHost(), LDAP_CONTAINER.getMappedPort(389))
+            });
+  }
+
+  @Autowired private WebTestClient webTestClient;
+
+  @Value("${jwt-auth.auth-path}")
+  private String loginPath;
+
+  @Autowired private LdapTemplate ldapTemplate;
+
+  @BeforeAll
+  static void initUser() throws Exception {
+    // 导入 LDIF 文件
+    System.out.println(
+        LDAP_CONTAINER
+            .execInContainer(
+                "ldapadd", "-x",
+                "-H", "ldap://localhost:389",
+                "-D", "cn=admin,dc=test,dc=com",
+                "-w", ADMIN_PASSWORD,
+                "-f", "/tmp/users.ldif")
+            .toString());
+
+    System.out.println(
+        LDAP_CONTAINER.execInContainer("slappasswd", "-h", "{SSHA}", "-s", "123456").toString());
+
+    System.out.println(
+        LDAP_CONTAINER.execInContainer("slappasswd", "-h", "{SSHA}", "-s", "abcdef").toString());
+
+    System.out.println(
+        LDAP_CONTAINER.execInContainer("slappasswd", "-h", "{SSHA}", "-s", "Jone@123").toString());
+  }
+
+  @Test
+  void testUserAuth() {
+    EqualsFilter filter = new EqualsFilter("uid", "tom");
+    assertTrue(ldapTemplate.authenticate("", filter.encode(), "123456"));
+  }
+
+  @Test
+  @Order(1)
+  void testRedisConnected() {
+    assertTrue(REDIS_CONTAINER.isRunning());
+  }
+
+  @Test
+  void testLogin() {
+    Credentials credentials = new Credentials();
+    credentials.setPassword("mypassword");
+    credentials.setUserName("sam");
+
+    TokenBody responseBody =
+        webTestClient
+            .post()
+            .uri(loginPath)
+            .body(BodyInserters.fromValue(credentials))
+            .accept(APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBody(TokenBody.class)
+            .returnResult()
+            .getResponseBody();
+
+    assertTrue(responseBody != null && hasLength(responseBody.getToken()));
+  }
+}
