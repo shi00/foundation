fd310d5c3bbce3b6fa80f2aadd0f3b9d36b9d3c8
louis sin <louis2sin@gmail.com>
2025-06-22 16:26:05 +0800

    chatbot首次合入

---
+++ LLMs/chatbot/src/main/java/com/silong/llm/chatbot/configure/DataSourceAutoConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.llm.chatbot.configure;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 数据源自动配置
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2025-05-19 20:21
+ */
+@Configuration
+public class DataSourceAutoConfiguration {
+
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource.hikari")
+  public HikariDataSource dataSource(DataSourceProperties properties) {
+    return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+}
