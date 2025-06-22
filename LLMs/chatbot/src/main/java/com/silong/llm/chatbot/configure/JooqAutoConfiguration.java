f7401525a34312d84019773077619424890fef60
louis sin <louis2sin@gmail.com>
2025-06-22 16:49:14 +0800

    chatbot首次合入

---
+++ LLMs/chatbot/src/main/java/com/silong/llm/chatbot/configure/JooqAutoConfiguration.java
@@ -0,0 +1,74 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.silong.llm.chatbot.configure;
+
+import javax.sql.DataSource;
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * jooq自动配置
+ *
+ * @author louis sin
+ * @version 1.0.0
+ * @since 2025-05-19 20:21
+ */
+@Configuration
+@EnableTransactionManagement
+public class JooqAutoConfiguration {
+
+  private DataSource dataSource;
+
+  @Bean
+  public DataSourceConnectionProvider connectionProvider() {
+    return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(dataSource));
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+  @Bean
+  public DefaultDSLContext dslContext() {
+    return new DefaultDSLContext(configuration());
+  }
+
+  private DefaultConfiguration configuration() {
+    DefaultConfiguration config = new DefaultConfiguration();
+    config.set(connectionProvider());
+    return config;
+  }
+
+  @Autowired
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+}
